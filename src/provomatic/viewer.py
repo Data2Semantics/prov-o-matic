337aff8b956059c8d9d31d51efb3b71acece94b6
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-03-12 14:42:10 +0100

    Working viewer

--- src/provomatic/viewer.py
+++ src/provomatic/viewer.py
@@ -4,8 +4,11 @@ from rdflib import Graph
 from IPython.display import HTML
 import hashlib
 import requests
+import os 
 
-PROVOVIZ_SERVICE = "http://semweb.cs.vu.nl/provoviz/service"
+#PROVOVIZ_SERVICE = "http://semweb.cs.vu.nl/provoviz/service"
+PROVOVIZ_SERVICE = "http://localhost:5000/service"
+
 
 def view_prov():
     graph = Graph()
@@ -15,13 +18,21 @@ def view_prov():
     
     graph_ttl = graph.serialize(format='turtle')
     
-    graph_uri = "http://provomatic.org/export/{}".format(hashlib.md5(graph_ttl).hexdigest())
+    digest = hashlib.md5(graph_ttl).hexdigest()
+    
+    graph_uri = "http://provomatic.org/export/{}".format(digest)
     
     payload = {'graph_uri': graph_uri, 'data': graph_ttl}
     response = requests.post(PROVOVIZ_SERVICE, data=payload)
     
-    print response.text
+    html_filename = '{}_provoviz.html'.format(digest)
+    html_file = open(html_filename,'w')
+    html_file.write(response.text)
+    html_file.close()
     
-    return HTML(response.text)
+    iframe = "<iframe width='100%' height='450px' src='http://localhost:8000/{}'></iframe>".format(html_filename)
+    
+    
+    return HTML(iframe)
     
     
