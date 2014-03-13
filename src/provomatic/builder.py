50ece2e310d4290239c0a568724b53f6c98c0006
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-03-13 16:50:00 +0100

    Different way of generating the prov graph: using plans as entities input

--- src/provomatic/builder.py
+++ src/provomatic/builder.py
@@ -36,16 +36,14 @@ class ProvBuilder(object):
         self.g.bind('skos',self.SKOS)
         
         
-    
-        self.g.add((plan_uri, RDF.type, RDFS['Class']))
-        self.g.add((plan_uri, RDF.type, self.PROVOMATIC['Method']))
-        self.g.add((plan_uri, RDFS.label, Literal(name)))
+        self.g.add((plan_uri, RDF.type, self.PROV['Plan']))
+        self.g.add((plan_uri, RDFS.label, Literal("{} Plan".format(name))))
         self.g.add((plan_uri,self.SKOS.note,Literal(description)))
         
     
         self.g.add((activity_uri,RDF.type,self.PROV['Activity']))
         self.g.add((activity_uri,RDFS.label,Literal(name)))
-        self.g.add((activity_uri,RDF.type,plan_uri))
+        self.g.add((activity_uri,self.PROV['used'],plan_uri))
         self.g.add((activity_uri,self.SKOS.note,Literal(description)))
     
         # For each input, create a 'used' relation
@@ -57,6 +55,7 @@ class ProvBuilder(object):
             self.g.add((activity_uri, self.PROV['used'], input_uri))
         
         # For each output, create a 'generated' relation
+        # Always expand tuples, to capture the variables separately.
         if isinstance(outputs, tuple) :
             count = 0
             for value in outputs:
@@ -67,6 +66,7 @@ class ProvBuilder(object):
                 output_uri = self.add_entity("{} output {}".format(name,count),vdigest,value)
             
                 self.g.add((activity_uri, self.PROV['generated'], output_uri))
+        # Only expand dictionaries when explicitly told to do so
         elif expand_output_dict and isinstance(outputs,dict):
             for oname, value in outputs.items() :
                 value, vdigest = self.get_value(value)
@@ -74,6 +74,7 @@ class ProvBuilder(object):
                 output_uri = self.add_entity(oname, vdigest, value)
                 
                 self.g.add((activity_uri, self.PROV['generated'], output_uri))
+        # Otherwise we'll take the value at 'face value'
         else :
             value, vdigest = self.get_value(outputs)
         
